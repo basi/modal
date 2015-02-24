451c1d2df82f4a197ffbab1f6f903510a2c32bca
basi <ishibashi.tomoyuki@gmail.com>
2015-02-25 03:44:05 +0900

    modal test

---
+++ MyFirstProject/MySecondViewController.h
@@ -0,0 +1,19 @@
+//
+//  MySecondViewController.h
+//  MyFirstProject
+//
+//  Created by yuichi.takeda on 1/17/15.
+//  Copyright (c) 2015 mixi, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol MySecondViewControllerDelegate <NSObject>
+- (void)secondViewControllerButtonTapped;
+@end
+
+@interface MySecondViewController : UIViewController <UIWebViewDelegate> {
+  IBOutlet UIWebView *picaWebView;
+}
+@property (nonatomic, weak) id<MySecondViewControllerDelegate> delegate;
+@end
